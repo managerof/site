eda8e4d56447ab640f3701f5e6841313815c9046
managerof <109286391+managerof@users.noreply.github.com>
2022-10-18 22:14:52 +0100

    test

---
+++ site/App/app.js
@@ -0,0 +1,14 @@
+document.getElementById("ans_button").addEventListener("click", sendAnswer); 
+
+function sendAnswer() {
+    let count = 0;
+    const ans1 = 4;
+    const ans2 = 16;
+    const ans3 = 3;
+    const ans4 = 125;
+    ans1 != document.getElementById("1").value ? count : count++;
+    ans2 == document.getElementById("2").value ? count++ : count;
+    ans3 == document.getElementById("3").value ? count++ : count;
+    ans4 == document.getElementById("4").value ? count++ : count;
+    alert(`${count} correct answers`);
+}
